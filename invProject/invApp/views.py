cd6eddccb63f109cab5b5636734206da745ecba9
elbse <changpriego@gmail.com>
2026-01-16 22:50:12 +0800

    templates

--- invProject/invApp/views.py
+++ invProject/invApp/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .forms import ProductForm
+from .models import Product
+
 
 # Create your views here.
+# CRUD
+
+
+# Home View 
+def home_view(request):
+    return render(request)
+# Create
+
+
+# Read View
+
+# Update View 
+
+# Delete View
